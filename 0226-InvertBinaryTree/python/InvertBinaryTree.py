d2bc6e38acd361542f599afe88a2f0ee7896d0b6
Guan-Wei Huang <gwhuang24@gmail.com>
2025-01-27 23:54:49 -0500

    Python version: O(n)

---
+++ 0226-InvertBinaryTree/python/InvertBinaryTree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        # swap the children
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+
