caf3b6749ea3f42e20b67c6ebac7582b8f347d10
Guan-Wei Huang <gwhuang24@gmail.com>
2025-02-25 12:14:19 -0500

    0019 Fix Memory Leak, C++ version: O(n)

--- 0019-RemoveNthNodeFromEndofList/cpp/RemoveNthNodeFromEndofList.cpp
+++ 0019-RemoveNthNodeFromEndofList/cpp/RemoveNthNodeFromEndofList.cpp
@@ -1,17 +1,13 @@
-#include <iostream>
-#include<vector>
-using namespace std;
 /**
  * Definition for singly-linked list.
-*/
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
@@ -20,58 +16,20 @@ public:
         ListNode* ptr_r = dummy;
         ListNode* ptr_l = dummy;
 
-        while(n-- && ptr_r != NULL){
+        for (int i = 0; i <= n; i++) {
             ptr_r = ptr_r->next;
         }
-        ptr_r = ptr_r->next;
+
         while(ptr_r){
             ptr_r = ptr_r->next;
             ptr_l = ptr_l->next;
         }
-        ptr_l->next = ptr_l->next->next;
-        return dummy->next;
+        ListNode* toDelete = ptr_l->next;
+        ptr_l->next = toDelete->next;
+        delete toDelete; 
+        
+        ListNode* newHead = dummy->next;
+        delete dummy;
+        return newHead;
     }
 };
-
-ListNode* createLinkedList(const vector<int>& nums) {
-    ListNode* dummy = new ListNode(0);
-    ListNode* current = dummy;
-    for (int num : nums) {
-        current->next = new ListNode(num);
-        current = current->next;
-    }
-    return dummy->next;
-}
-
-// Helper function to print a linked list
-void printLinkedList(ListNode* head) {
-    ListNode* current = head;
-    while (current) {
-        cout << current->val;
-        if (current->next) cout << " -> ";
-        current = current->next;
-    }
-    cout << endl;
-}
-
-int main() {
-    Solution solution;
-
-    // Create a linked list from a vector
-    vector<int> nums = {1, 2, 3, 4, 5};
-    ListNode* head = createLinkedList(nums);
-
-    // Print the original linked list
-    cout << "Original Linked List: ";
-    printLinkedList(head);
-
-    // Remove the 2nd node from the end
-    int n = 2;
-    head = solution.removeNthFromEnd(head, n);
-
-    // Print the modified linked list
-    cout << "Modified Linked List: ";
-    printLinkedList(head);
-
-    return 0;
-}
